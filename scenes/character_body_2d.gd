62e94cfee57c3d27c0967618513107f121db7a9a
TheBiggerMoo <elliott.groeneweg@gmail.com>
2025-11-10 18:53:04 +0100

    crouching endoskeleton and input map keys

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -11,14 +11,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left_arrow", "right_arrow")
 	if direction:
 		velocity.x = direction * SPEED
+		if Input.is_action_pressed("space") and is_on_floor():
+			print("banana")
+			velocity.x = velocity.x * 0.5
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
